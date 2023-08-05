8fdd9062bc77a26af2b8db8d8d24bc7b5ddbe7b2
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-08-06 02:48:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[username,setUsername]=useState("")
+  let[pass,setPass]=useState("")
+  let[err,setErr]=useState("")
+  function change(e){
+    e.preventDefault()
+    if(!username || !pass){
+      setErr("Both username and password are required.")
+    }
+  }
   return (
-    <div>
+    <div className="app">
         {/* Do not remove the main div */}
+    <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} />
+     <input type="password" value={pass} onChange={(e)=>setPass(e.target.value)} />
+      <p></p>
+{err&&<p id="errorMessage">{err}</p>}
+       <button onClick={change}>Login</button>
     </div>
   )
 }
 
-export default App
+export default App;
